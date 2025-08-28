ad7044cec0bb4c07d7b98be5962f88e532b4e5ac
Annalis Khemphomma <akhemphomma2@huskers.unl.edu>
2025-08-28 09:44:51 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Annalis Khemphomma
+ *         akhemphomma2@huskers.unl.edu
+ * Date: 8/26/2025
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Annalis Khemphomma\n");
+
+  return 0;
+}
